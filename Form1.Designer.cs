d284a0b74da6be25d96565c5ad5f5654c853fa8a
Dimitris Georgiadis <zibraogi@gmail.com>
2021-01-14 02:02:36 +0200

    c# crud

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace application
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createdByDimitrisGeorgidisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ListBox
+            // 
+            this.ListBox.BackColor = System.Drawing.Color.White;
+            this.ListBox.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ListBox.ForeColor = System.Drawing.Color.Black;
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 24;
+            this.ListBox.Location = new System.Drawing.Point(378, 42);
+            this.ListBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(287, 268);
+            this.ListBox.TabIndex = 0;
+            this.ListBox.SelectedIndexChanged += new System.EventHandler(this.ListBox_SelectedIndexChanged);
+            // 
+            // TextBox
+            // 
+            this.TextBox.BackColor = System.Drawing.Color.White;
+            this.TextBox.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.TextBox.ForeColor = System.Drawing.Color.Black;
+            this.TextBox.Location = new System.Drawing.Point(50, 42);
+            this.TextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(297, 34);
+            this.TextBox.TabIndex = 1;
+            this.TextBox.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn_add.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_add.Enabled = false;
+            this.btn_add.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.btn_add.ForeColor = System.Drawing.Color.Black;
+            this.btn_add.Location = new System.Drawing.Point(93, 99);
+            this.btn_add.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(185, 40);
+            this.btn_add.TabIndex = 2;
+            this.btn_add.Text = "&Add";
+            this.btn_add.UseVisualStyleBackColor = false;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.Color.Moccasin;
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.menu.Size = new System.Drawing.Size(687, 26);
+            this.menu.TabIndex = 6;
+            this.menu.Text = "menu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.fileToolStripMenuItem.Text = "file";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_delete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_delete.Enabled = false;
+            this.btn_delete.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.btn_delete.ForeColor = System.Drawing.Color.Black;
+            this.btn_delete.Location = new System.Drawing.Point(93, 157);
+            this.btn_delete.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(185, 40);
+            this.btn_delete.TabIndex = 7;
+            this.btn_delete.Text = "&Delete";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.btn_update.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_update.Enabled = false;
+            this.btn_update.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.btn_update.ForeColor = System.Drawing.Color.Black;
+            this.btn_update.Location = new System.Drawing.Point(93, 213);
+            this.btn_update.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(185, 40);
+            this.btn_update.TabIndex = 8;
+            this.btn_update.Text = "&Update";
+            this.btn_update.UseVisualStyleBackColor = false;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.BackColor = System.Drawing.Color.AliceBlue;
+            this.btn_clear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_clear.Enabled = false;
+            this.btn_clear.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.btn_clear.ForeColor = System.Drawing.Color.Black;
+            this.btn_clear.Location = new System.Drawing.Point(93, 270);
+            this.btn_clear.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(185, 40);
+            this.btn_clear.TabIndex = 9;
+            this.btn_clear.Text = "&Clear";
+            this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createdByDimitrisGeorgidisToolStripMenuItem});
+            this.aboutToolStripMenuItem.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.aboutToolStripMenuItem.Text = "about";
+            // 
+            // createdByDimitrisGeorgidisToolStripMenuItem
+            // 
+            this.createdByDimitrisGeorgidisToolStripMenuItem.Name = "createdByDimitrisGeorgidisToolStripMenuItem";
+            this.createdByDimitrisGeorgidisToolStripMenuItem.Size = new System.Drawing.Size(262, 22);
+            this.createdByDimitrisGeorgidisToolStripMenuItem.Text = "Created by Dimitris Georgidis";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(687, 335);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.menu);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menu;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "List";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createdByDimitrisGeorgidisToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace application
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            updateform();
+        }
+
+        void updateform()
+        {
+            btn_add.Enabled = !String.IsNullOrWhiteSpace(TextBox.Text);
+            btn_delete.Enabled = (ListBox.Items.Count > 0) && (ListBox.SelectedIndex >= 0);
+            btn_update.Enabled = (ListBox.Items.Count > 0) && (ListBox.SelectedIndex >= 0);
+            btn_clear.Enabled = (ListBox.Items.Count > 0);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            ListBox.Items.Add(TextBox.Text);
+            TextBox.Clear();
+            updateform();
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            btn_add.Enabled = !String.IsNullOrWhiteSpace(TextBox.Text);
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            ListBox.Items.RemoveAt(ListBox.SelectedIndex);
+            updateform();
+        }
+
+        private void ListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            updateform();
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            ListBox.Items.Insert(ListBox.SelectedIndex, TextBox.Text);
+            ListBox.Items.RemoveAt(ListBox.SelectedIndex);
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            ListBox.Items.Clear();
+            updateform();
+        }
+    }
+}
